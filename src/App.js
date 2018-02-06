79484d3e0dbc0927efa428f19dc9e5f664f49b57
Brandon Lew <brandon.t.lew@gmail.com>
2018-02-05 22:21:46 -0800

    clean production code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 
-
+const photos = ["banner1.jpg","banner2.jpg","banner3.jpg","banner4.jpg","banner5.jpg"];
+const random = Math.floor(Math.random() * photos.length);
+const bannerStyle = {backgroundImage: 'url(img/' + photos[random] + ')'}
 
 class App extends Component {
 
   render() {
-    const photos = ["banner1.jpg","banner2.jpg","banner3.jpg","banner4.jpg","banner5.jpg"];
-    const random = Math.floor(Math.random() * photos.length);
-    const bannerStyle = {backgroundImage: 'url(' + photos[random] + ')'}
+
     return (
       <div className="banner" style={bannerStyle}>
         <div className="overlay"></div>
