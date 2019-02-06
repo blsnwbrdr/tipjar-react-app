ece0535b115e844ce4e76875913f57f7c44dd6e9
Brandon Lew <brandon.t.lew@gmail.com>
2019-02-05 20:26:15 -0800

    change copyright date

--- src/screens/ListingsFooter.js
+++ src/screens/ListingsFooter.js
@@ -6,7 +6,7 @@ class ListingsFooter extends Component {
     return (
       <footer className='footer'>
         <p><a href='mailto:brandon.t.lew@gmail.com'>brandon.t.lew@gmail.com</a></p>
-        <p>&copy; 2018 <a href='http://brandonsco.de' target='_blank' rel="noopener noreferrer">brandonsCO.DE</a></p>
+        <p>&copy; 2019 <a href='http://brandonsco.de' target='_blank' rel="noopener noreferrer">brandonsCO.DE</a></p>
       </footer>
     )
   }
