832c6192054e0e8e346c14ee54b13dfaeb7f1075
Brandon Lew <brandon.t.lew@gmail.com>
2023-01-27 19:10:24 -0500

    change copyright year to date method

--- src/screens/ListingsFooter.js
+++ src/screens/ListingsFooter.js
@@ -8,7 +8,7 @@ class ListingsFooter extends Component {
           <a href="mailto:brandon.t.lew@gmail.com">brandon.t.lew@gmail.com</a>
         </p>
         <p>
-          &copy; 2020{' '}
+          &copy; {new Date().getFullYear()}{' '}
           <a
             href="http://brandonlew.com"
             target="_blank"
