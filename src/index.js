1a16e7b8a2b76a55a7e9abb28ab2fb359950e277
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-14 15:14:57 -0700

    delete registerServiceWorker code

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './styles/index.min.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
 
 const REACT_VERSION = React.version;
 
@@ -10,4 +9,3 @@ const container = document.getElementById('app');
 container.setAttribute('react-version', REACT_VERSION);
 const root = createRoot(container);
 root.render(<App />);
-// registerServiceWorker();
