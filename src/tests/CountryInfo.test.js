073bc9cccd16a77d47e827b45a5aa1ddb8cacfbf
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-21 11:00:29 -0700

    create data test

--- src/tests/CountryInfo.test.js
+++ src/tests/CountryInfo.test.js
@@ -20,3 +20,37 @@ it('renders CountryInfo component', async () => {
     );
   });
 });
+
+it('renders data', async () => {
+  const container = document.createElement('div');
+  document.body.appendChild(container);
+
+  const countryTipData = {
+    country: 'country',
+    dining: 'dining',
+    transportation: 'transportation',
+    accommodation: 'accommodation',
+    currency: 'currency',
+    thankyou: 'thankyou',
+    goodbye: 'goodbye',
+  };
+  const countryCurrencyData = '2.5';
+
+  await act(async () => {
+    ReactDOM.createRoot(container).render(
+      <CountryInfo
+        countryTipData={countryTipData}
+        countryCurrencyData={countryCurrencyData}
+      />
+    );
+  });
+  const h2 = container.querySelector('h2');
+  const p = container.querySelectorAll('p');
+  expect(h2.textContent).toBe('country');
+  expect(p[0].textContent).toBe('dining');
+  expect(p[1].textContent).toBe('transportation');
+  expect(p[2].textContent).toBe('accommodation');
+  expect(p[3].textContent).toContain('2.5 currency');
+  expect(p[4].textContent).toBe('thankyou');
+  expect(p[5].textContent).toBe('goodbye');
+});
