33d7b069141c681c9728aafbdf1d2e2ff1fd7e3d
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-21 11:24:56 -0700

    reorganize index

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client';
+import ReactDom from 'react-dom/client';
 import './styles/index.min.css';
 import App from './App';
 
-const REACT_VERSION = React.version;
-
 const container = document.getElementById('app');
+
+// Set React version in app element
+const REACT_VERSION = React.version;
 container.setAttribute('react-version', REACT_VERSION);
-const root = createRoot(container);
+
+const root = ReactDom.createRoot(container);
 root.render(<App />);
