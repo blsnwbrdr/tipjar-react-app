dd23cccf164af226f937c60d73aa7147d676283e
Brandon Lew <25602300+blsnwbrdr@users.noreply.github.com>
2024-08-30 14:59:38 -0700

    remove colon from titles

--- src/screens/CountryInfo.js
+++ src/screens/CountryInfo.js
@@ -12,24 +12,24 @@ class CountryInfo extends Component {
         <div className='country'>
           <h2>{this.props.countryTipData.country}</h2>
           <div className='fa fa-cutlery'></div>
-          <h3 className='line-100'>Dining*:</h3>
+          <h3 className='line-100'>Dining*</h3>
           <p>{this.props.countryTipData.dining}</p>
           <div className='fa fa-taxi'></div>
-          <h3 className='line-200'>Transportation:</h3>
+          <h3 className='line-200'>Transportation</h3>
           <p>{this.props.countryTipData.transportation}</p>
           <div className='fa fa-building'></div>
-          <h3 className='line-200'>Accommodation:</h3>
+          <h3 className='line-200'>Accommodation</h3>
           <p>{this.props.countryTipData.accommodation}</p>
           <div className='fa fa-money'></div>
-          <h3 className='line-140'>Currency**:</h3>
+          <h3 className='line-140'>Currency**</h3>
           <p>
             1 USD = {this.props.countryCurrencyData}{' '}
             {this.props.countryTipData.currency}
           </p>
           <div className='fa fa-language'></div>
-          <h3 className='line-140'>Thank you:</h3>
+          <h3 className='line-140'>Thank you</h3>
           <p>{this.props.countryTipData.thankyou}</p>
-          <h3 className='line-125'>Goodbye:</h3>
+          <h3 className='line-125'>Goodbye</h3>
           <p>{this.props.countryTipData.goodbye}</p>
           <p className='disclaimer'>
             *If paying with a credit card, it is highly recommended to have cash
